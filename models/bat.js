e92bc18c80aabb24f4ee090fcf18ac9081a8d860
KalpanaBolla <123128148+KalpanaBolla@users.noreply.github.com>
2023-04-27 23:59:37 -0500

    fiinal commit

--- models/bat.js
+++ models/bat.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 const batSchema = mongoose.Schema({
 bat_type: String,
-bat_size: Number,
-bat_cost: Number
+bat_size: {type: Number, min:3},
+bat_cost: {type: Number, maxLength:15}
 })
 module.exports = mongoose.model("Bat",batSchema)
--- routes/bat.js
+++ routes/bat.js
@@ -2,15 +2,22 @@ var express = require('express');
 const bat_controlers= require('../controllers/bat');
 var router = express.Router();
 
+const secured = (req, res, next) => {
+    if (req.user){
+    return next();
+    }
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login");
+    }
 
 /* Get bat */
 router.get('/', bat_controlers.bat_view_all_Page );
 /* GET detail bat page */
 router.get('/detail', bat_controlers.bat_view_one_Page);
 /* GET create bat page */
-router.get('/create', bat_controlers.bat_create_Page);
+router.get('/create', secured, bat_controlers.bat_create_Page);
 /* GET update bat page */
-router.get('/update',  bat_controlers.bat_update_Page);
+router.get('/update', secured, bat_controlers.bat_update_Page);
 /* GET delete bat page */
-router.get('/delete', bat_controlers.bat_delete_Page);
+router.get('/delete', secured, bat_controlers.bat_delete_Page);
 module.exports = router;
